72dda8f88a25e45cfb4e1a21bed5579d5e8f2f3b
fxrebxrn <olegsup35@gmail.com>
2026-04-30 17:26:59 +0300

    refactor: move authentication logic to AuthService and clean up routes

--- routers/auth.py
+++ routers/auth.py
@@ -1,17 +1,13 @@
-from fastapi import HTTPException, APIRouter, Depends
+from fastapi import APIRouter, Depends
 from schemas.user_schemas import UserRegister, RefreshTokenRequest
-from models import User
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import get_db
-from core.security import hash_password, verify_password, create_access_token, decode_token, create_refresh_token
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi import Request
-from utils.rate_limit import check_failed_login_limit, add_failed_login_attempt, reset_failed_login_attempts
 from typing import Annotated
 from schemas.user_schemas import MessageWithUser
 from schemas.util_schemas import TokenOutResponse, RefreshTokenOutResponse
-from core.exceptions import InvalidTokenError
-from services.repositories.user_repository import UserRepository
+from services.auth_service import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -19,75 +15,15 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 @router.post("/register", response_model=MessageWithUser)
 async def register_user(user: UserRegister, db: Annotated[AsyncSession, Depends(get_db)]):
-    service = UserRepository(db)
-    existing_user = await service.get_user_by_email(user.email)
-
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email already exists")
-    
-    hashed_password = hash_password(user.password)
-
-    new_user = User(
-        name=user.name,
-        age=user.age,
-        email=user.email,
-        hashed_password=hashed_password
-    )
-
-    db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user)
-
-    return {
-        "message": "User registered successfully",
-        "user": new_user
-    }
+    service = AuthService(db)
+    return await service.register_user(user)
 
 @router.post("/login", response_model=TokenOutResponse)
 async def login_user(request: Request, db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    email = form_data.username
-    password = form_data.password
-    
-    ip = request.client.host
-    key = f"failed_login:{ip}:{email}"
-    await check_failed_login_limit(key, limit=5)
-
-    service = UserRepository(db)
-    user_to_login = await service.get_user_by_email(email)
-
-    if not user_to_login:
-        await add_failed_login_attempt(key, window=60)
-        raise HTTPException(status_code=401, detail="Incorrect credentials")
-
-    if not verify_password(password, user_to_login.hashed_password):
-        await add_failed_login_attempt(key, window=60)
-        raise HTTPException(status_code=401, detail="Incorrect credentials")
-    
-    await reset_failed_login_attempts(key)
-
-    access_token = create_access_token({"user_id": user_to_login.id})
-    refresh_token = create_refresh_token({"user_id": user_to_login.id})
-    
-    return {
-        "access_token": access_token,
-        "refresh_token": refresh_token,
-        "token_type": "bearer"
-    }
+    service = AuthService(db)
+    return await service.login_user(request, form_data)
 
 @router.post("/refresh", response_model=RefreshTokenOutResponse)
 async def refresh_token(data: RefreshTokenRequest):
-    payload = decode_token(data.refresh_token)
-
-    if payload.get("type") != "refresh":
-        raise InvalidTokenError()
-    
-    user_id = payload.get("user_id")
-    if not user_id:
-        raise InvalidTokenError()
-    
-    new_access_token = create_access_token({"user_id": user_id})
-
-    return {
-        "access_token": new_access_token,
-        "token_type": "bearer"
-    }
+    service = AuthService()
+    return await service.refresh_token(data)
---
+++ services/auth_service.py
@@ -0,0 +1,88 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from services.user_service import UserService
+from services.repositories.base_repository import BaseRepository
+from schemas.user_schemas import UserRegister
+from core.security import hash_password, verify_password, create_access_token, create_refresh_token, decode_token
+from fastapi import HTTPException, Request
+from models import User
+from services.repositories.user_repository import UserRepository
+from fastapi.security import OAuth2PasswordRequestForm
+from utils.rate_limit import check_failed_login_limit, add_failed_login_attempt, reset_failed_login_attempts
+from core.exceptions import InvalidTokenError
+from schemas.user_schemas import RefreshTokenRequest
+
+class AuthService:
+    def __init__(self, db: AsyncSession = None):
+        self.db = db
+        self.user = UserService(db)
+        self.base_repo = BaseRepository(db)
+        self.user_repo = UserRepository(db)
+
+    async def register_user(self, user: UserRegister):
+        existing_user = await self.user_repo.get_user_by_email(user.email)
+
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Email already exists")
+        
+        hashed_password = hash_password(user.password)
+
+        new_user = User(
+            name=user.name,
+            age=user.age,
+            email=user.email,
+            hashed_password=hashed_password
+        )
+
+        await self.base_repo.add(new_user)
+
+        return {
+            "message": "User registered successfully",
+            "user": new_user
+        }
+
+    async def login_user(self, request: Request, form_data: OAuth2PasswordRequestForm):
+        email = form_data.username
+        password = form_data.password
+        
+        ip = request.client.host
+        key = f"failed_login:{ip}:{email}"
+        await check_failed_login_limit(key, limit=5)
+
+        user_to_login = await self.user_repo.get_user_by_email(email)
+
+        if not user_to_login:
+            await add_failed_login_attempt(key, window=60)
+            raise HTTPException(status_code=401, detail="Incorrect credentials")
+
+        if not verify_password(password, user_to_login.hashed_password):
+            await add_failed_login_attempt(key, window=60)
+            raise HTTPException(status_code=401, detail="Incorrect credentials")
+        
+        await reset_failed_login_attempts(key)
+
+        access_token = create_access_token({"user_id": user_to_login.id})
+        refresh_token = create_refresh_token({"user_id": user_to_login.id})
+        
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "bearer"
+        }
+    
+    async def refresh_token(self, data: RefreshTokenRequest):
+        payload = decode_token(data.refresh_token)
+
+        if payload.get("type") != "refresh":
+            raise InvalidTokenError()
+        
+        user_id = payload.get("user_id")
+        if not user_id:
+            raise InvalidTokenError()
+        
+        new_access_token = create_access_token({"user_id": user_id})
+
+        return {
+            "access_token": new_access_token,
+            "token_type": "bearer"
+        }
+    
